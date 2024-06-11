cf1c2d7f795099432fccd293498968f21780aed2
Ye PENG <37892536+peng-ye@users.noreply.github.com>
2024-06-11 16:50:34 +0800

    Update utils.R

--- utils.R
+++ utils.R
@@ -431,6 +431,25 @@ format_p_list <- function(x) {
 	return(sapply(x, format_p))
 }
 
+# for table1
+# https://cran.r-project.org/web/packages/table1/vignettes/table1-examples.html
+pvalue <- function(x, ...) {
+    # Construct vectors of data y, and groups (strata) g
+    y <- unlist(x)
+    g <- factor(rep(1:length(x), times=sapply(x, length)))
+    if (is.numeric(y)) {
+        # For numeric variables, perform a standard 2-sample t-test
+        p <- t.test(y ~ g)$p.value
+    } else {
+        # For categorical variables, perform a chi-squared test of independence
+        p <- chisq.test(table(y, g))$p.value
+    }
+    # Format the p-value, using an HTML entity for the less-than sign.
+    # The initial empty string places the output on the line below the variable label.
+    c("", sub("<", "&lt;", format.pval(p, digits=3, eps=0.001)))
+}
+
+
 # return the last n characters
 substrRight <- function(x, n){
   substr(x, nchar(x)-n+1, nchar(x))
