9b4d45d30aacaa6cd157b4dfb148273dd82bf6a0
Ye PENG <37892536+peng-ye@users.noreply.github.com>
2024-08-24 15:40:55 +0800

    Update utils.R
    add adonis_parOmegaR2_fastadonis

--- utils.R
+++ utils.R
@@ -48,6 +48,44 @@ adonis.pair<-function(dist.mat,Factor,nper=1000,corr.method="fdr"){
   data.frame(combination=paste(comb.fact[1,],comb.fact[2,],sep=" <-> "),SumsOfSqs=SS,MeanSqs=MeanSqs,F.Model=F.Model,R2=R2,P.value=pv,P.value.corrected=pv.corr)}
 # https://rdrr.io/github/GuillemSalazar/EcolUtils/src/R/EcolUtils_functions.R
 
+#' Calculate (partial) Omega-squared (effect-size calculation) for PERMANOVA and add it to the input object
+#'
+#' @param adonisOutput An adonis object
+#' @param partial Should partial omega-squared be calculated (sample size adjusted). Default TRUE
+#' @return Original adonis object with the (partial) Omega-squared values added
+#' @import vegan
+#' @export
+adonis_OmegaSq_fastAdonis <- function(adonisOutput, partial = TRUE){
+    aov_tab <- adonisOutput$aov.tab
+    
+    aov_tab$MeanSqs <- aov_tab$SumsOfSqs / aov_tab$Df
+    aov_tab$MeanSqs[length(aov_tab$Df)] <- NA
+
+    heading <- attr(aov_tab, "heading")
+    MS_res <- aov_tab[pmatch("Residual", rownames(aov_tab)), "MeanSqs"]
+    SS_tot <- aov_tab[rownames(aov_tab) == "Total", "SumsOfSqs"]
+    N <- aov_tab[rownames(aov_tab) == "Total", "Df"] + 1
+    if(partial){
+        omega <- apply(aov_tab, 1, function(x) (x["Df"]*(x["MeanSqs"]-MS_res))/(x["Df"]*x["MeanSqs"]+(N-x["Df"])*MS_res))
+        aov_tab$parOmegaSq <- c(omega[1:(length(omega)-2)], NA, NA)
+    } else {
+        omega <- apply(aov_tab, 1, function(x) (x["SumsOfSqs"]-x["Df"]*MS_res)/(SS_tot+MS_res))
+        aov_tab$OmegaSq <- c(omega[1:(length(omega)-2)], NA, NA)
+    }
+    if (is(adonisOutput, "adonis"))
+        cn_order <- c("Df", "SumsOfSqs", "MeanSqs", "F.Model", "R2",
+                      if (partial) "parOmegaSq" else "OmegaSq", "Pr(>F)")
+    else
+        cn_order <- c("Df", "SumOfSqs", "F", if (partial) "parOmegaSq" else "OmegaSq",
+                      "Pr(>F)")
+    aov_tab <- aov_tab[, cn_order]
+    attr(aov_tab, "names") <- cn_order
+    attr(aov_tab, "heading") <- heading
+    adonisOutput <- aov_tab
+    return(adonisOutput)
+}
+# adopted from https://raw.githubusercontent.com/Russel88/MicEco/master/R/adonis_OmegaSq.R
+
 hash_lineage <- function(x_, hash_) { # hash_ <- hash()
   a <- strsplit(x_, ";?\\S__")[[1]][-1]
   for (i in 2:length(a)) {
